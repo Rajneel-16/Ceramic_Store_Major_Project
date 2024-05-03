4dd44a64304021f6c45e725bfb81c6c32c9b538c
Rajneel-16 <rajneelwagh@gmail.com>
2024-05-03 20:43:02 +0530

    no changes made

--- website/settings.py
+++ website/settings.py
@@ -138,6 +138,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mr.goku.0619@gmail.com'
+EMAIL_HOST_USER = 'rajneelwagh@gmail.com'
 EMAIL_HOST_PASSWORD = 'lsmo ilbf amca vrec'
 EMAIL_USE_TLS = True
